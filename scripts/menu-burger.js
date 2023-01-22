4380649cd659bc89e8ba8d806541804c3323377e
Pessinaaa <pedro.pessina99@gmail.com>
2023-01-21 23:16:13 -0300

    estrutura html, css e modelo para mobile :construction:

---
+++ scripts/menu-burger.js
@@ -0,0 +1,24 @@
+const iconBurger = document.getElementById('burger')
+const menuList = document.getElementById('menu-list')
+const navLinks = document.querySelectorAll('#menu #menu-list li a')
+
+function toggleMenu(event){
+    if (event.type === 'touchstart') event.preventDefault();
+    iconBurger.classList.toggle('fechar')
+    menuList.classList.toggle('mostrar-menu')
+    const active = menuList.classList.contains('mostrar-menu')
+    event.currentTarget.setAttribute('aria-expanded', active)
+    if(active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir menu')
+    }
+}
+
+iconBurger.addEventListener('click', toggleMenu);
+iconBurger.addEventListener('touchstart', toggleMenu);
+
+navLinks.forEach(navLink => navLink.addEventListener('click', () => {
+    menuList.classList.remove('mostrar-menu');
+    iconBurger.classList.toggle('fechar')
+}));
