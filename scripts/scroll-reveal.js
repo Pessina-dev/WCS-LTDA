f2ed86916aa5aeb857eca9691afb91ee152dc226
Pessinaaa <pedro.pessina99@gmail.com>
2023-01-23 20:25:03 -0300

    :sparkles: animações :ambulance: troca do email

---
+++ scripts/scroll-reveal.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal({reset: false})
+
+sr.reveal('.logo-reveal',{
+    distance: '50%',
+    origin: 'bottom',
+    duration: 1000
+})
+
+sr.reveal('.name-reveal',{
+    distance: '50%',
+    origin: 'bottom',
+    duration: 1000,
+    delay: 250
+})
+
+sr.reveal('.title-reveal', {
+    distance: '20%',
+    origin: 'right',
+    duration: 1000
+})
+
+sr.reveal('.reveal', {
+    distance: '20%',
+    origin: 'right',
+    duration: 1000,
+    delay: 500
+})
